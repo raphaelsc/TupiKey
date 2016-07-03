eb96cce19f9593d82e7ec4bdfa9a5296d23d96c3
ScrimForever <scrimforever@gmail.com>
2016-07-02 21:07:35 -0300

    Primeira Mudança
    Change 1

--- TupiKey.py
+++ TupiKey.py
@@ -1,8 +1,8 @@
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 #                                       #
-#       Author: Thiago Salgado          #
+#       Project:                        #
 #                                       #
-#   Job: IT Security - UCAM CAMPOS      #
+#                                       #
 #                                       #
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 
@@ -14,217 +14,51 @@ import time
 
 def binar(letra):
 
-    if letra == "a":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-        #keyboard = 
-    elif letra == "b":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "c":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "d":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "e":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "f":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "g":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "h":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "i":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "j":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "k":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "l":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "m":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "n":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "o":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "p":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "q":
-        change = bin(ord(letra))
-        numb = change.replace("b","")   
-    elif letra == "r":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "s":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "t":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "u":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "v":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "w":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "x":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "y":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "z":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "A":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "B":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "C":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "D":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "E":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "F":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "G":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "H":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "I":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "J":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "K":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "L":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "M":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "N":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "O":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "P":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "Q":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "R":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "S":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "T":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "U":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "V":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "W":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "X":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "Y":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "Z":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "1":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "2":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "3":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "4":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "5":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "6":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "7":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "8":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "9":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    elif letra == "0":
-        change = bin(ord(letra))
-        numb = change.replace("b","")
-    return (numb)
+    mudar = bin(ord(letra))
+    binario = mudar.replace("b","")
+    return binario
 
 def startkey(x):
     print x
-    if x[0] == 0:
-        #send = ("""{NUMLOCK}{CAPSLOCK}{SCROLLLOCK}""")
-        numlock = "{NUMLOCK}"
-    elif x[0] == 1:
-        numlock = None
-    if x[1] == 0:
-        capslock = "{CAPSLOCK}"
-        
-    elif x[1] == 1:
-        capslock = None
-    if x[2] == 0:
-        scrolllock = "{SCROLLLOCK}"
-        
-    elif x[2] == 1:
-        scrollock = None
+    try:
+        if x[0] == 0:
+            
+            numlock = "{NUMLOCK}"
+            
+        elif x[0] == 1:
 
-    
+            numlock = "none"
+            
+        if x[1] == 0:
+
+            capslock = "{CAPSLOCK}"
+        
+        elif x[1] == 1:
+
+            capslock = "none"
+
+        if x[2] == 0:
+
+            scrolllock = "{SCROLLLOCK}"
+        
+        elif x[2] == 1:
+
+            scrollock = "none"
+
+    except e:
+
+        print(e)
+
+    if (numlock == "none" and capslock == "none" and scrolllock == "none"):
+        print ""
+    else:
+        print ""
+
     k = ("\"\"\""+numlock+capslock+scrolllock+"\"\"\"")
+
     print k
-    SendKeys.SendKeys(k,pause=0)
+
+    SendKeys.SendKeys(k,pause='0')
     
         
 def getLED():
@@ -254,10 +88,10 @@ def closekey(x):
         ""
     
 
-startkey(getLED())
+#startkey(getLED())
 entrada = raw_input("")
-time.sleep(3)
-closekey(getLED())
+#time.sleep(3)
+#closekey(getLED())
 print binar(entrada)
 #send = ("""{NUMLOCK 0}{CAPSLOCK 0}{SCROLLLOCK 0}""")
 #SendKeys.SendKeys(send,pause=0)
